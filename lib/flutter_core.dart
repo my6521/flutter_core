dea6e648f41ddee0ea86b30621eef631e23bbe30
王文兵 <22379099@qq.com>
2019-04-01 14:03:03 +0800

    webview

--- lib/flutter_core.dart
+++ lib/flutter_core.dart
@@ -8,6 +8,8 @@ export 'package:flutter_core/src/common/screen_util.dart';
 export 'package:flutter_core/src/common/regex_util.dart';
 export 'package:flutter_core/src/common/timer_util.dart';
 export 'package:flutter_core/src/common/object_util.dart';
+export 'package:flutter_core/src/widgets/swiper.dart';
+export 'package:flutter_core/src/widgets/web_scaffold.dart';
 
 /// A Calculator.
 class Calculator {
